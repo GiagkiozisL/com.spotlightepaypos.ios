73065a160261f1cb7ae6d3f044b2d2e4fb29ee81
GiagkiozisL <giagkiozis.l@hotmail.com>
2023-01-27 13:33:39 +0200

    clean code

--- Spotlight ePOS/ViewController.swift
+++ Spotlight ePOS/ViewController.swift
@@ -48,89 +48,13 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    
-    //        Crypto()
-    // Do any additional setup after loading the view.
-    
-    //        var proximityHepler = ProximityHelper()
-    
-    //        let txnType = "020000"
-    //        let amount = 4599
-    //        let storeId = "4001"
-    //        let orderId = "467eecc6-e3ac-4dc7-9418-e038d3491f79"
-    //        let secret = calulateHmac(txn_type: txnType, amount: amount, store_id: storeId, order_id: orderId)
-    
-    //    let key = "VerySecret"
-    //    let msg = "0200000000000045994001467eecc6-e3ac-4dc7-9418-e038d3491f79"
-    //    print("do hmac1: \(doHmac(key: key, msg: msg))")
-    //    print("do hmac2: \(doHmac(key: key.MD5, msg: msg))")
-    //    let bytes: Array<UInt8> = key.bytes
-    //    let digest = Digest.md5(bytes)
-    //    print("do hmac3: \(doHmac(key: digest.toHexString(), msg: msg))")
-    //    print("do hmac4: \(mac(secretKey: digest.toHexString(), message: msg))")
-    //    //
-    //
-    //    func mac(secretKey: String, message: String) -> String {
-    //        let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
-    //        let mac = UnsafeMutablePointer<CChar>.allocate(capacity: digestLength)
-    //
-    //        let cSecretKey: [CChar]? = secretKey.cString(using: .utf8)
-    //        let cSecretKeyLength = secretKey.lengthOfBytes(using: .utf8)
-    //
-    //        let cMessage: [CChar]? = message.cString(using: .utf8)
-    //        let cMessageLength = message.lengthOfBytes(using: .utf8)
-    //
-    //        CCHmac(CCHmacAlgorithm(kCCHmacAlgSHA256), cSecretKey, cSecretKeyLength, cMessage, cMessageLength, mac)
-    //
-    //        let macData = Data(bytes: mac, count: digestLength)
-    //
-    //        return macData.base64EncodedString()
-    //    }
-    //
-        func calulateHmac(txn_type: String, amount: Int, store_id: String, order_id: String) -> String {
-            let zeroPadded = String(format: "%012d", amount)
-            let hmacData = txn_type
-            + zeroPadded
-            + store_id
-            + order_id
-
-            return hmacData
-        }
-    //
-    //    func doHmac(key: String, msg: String) -> String {
-    //        do {
-    //            let hmacBts = try CryptoSwift.HMAC(key: key, variant: .sha256).authenticate(msg.bytes)
-    //            let data = Data(hmacBts)
-    //            return data.toHexString()
-    //        } catch {
-    //            print("Hash computation failed")
-    //        }
-    //        return ""
-    //    }
-    //
-    //
-    //    func convert64EncodedToHex(_ data:Data) -> String {
-    //        return data.map{ String(format: "%02x", $0) }.joined()
-    //    }
-    //
-    //    // CommonCrypto HMAC SHA256
-    //    //    func mac(secretKey: String, message: String) -> String {
-    //    //        let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
-    //    //        let mac = UnsafeMutablePointer<CChar>.allocate(capacity: digestLength)
-    //    //
-    //    //        let cSecretKey: [CChar]? = secretKey.cString(using: .utf8)
-    //    //        let cSecretKeyLength = secretKey.lengthOfBytes(using: .utf8)
-    //    //
-    //    //        let cMessage: [CChar]? = message.cString(using: .utf8)
-    //    //        let cMessageLength = message.lengthOfBytes(using: .utf8)
-    //    //
-    //    //        CCHmac(CCHmacAlgorithm(kCCHmacAlgSHA256), cSecretKey, cSecretKeyLength, cMessage, cMessageLength, mac)
-    //    //
-    //    //        let data = Data(bytes: mac, count: digestLength)
-    //    //
-    //    //        return convert64EncodedToHex(data)// macData.base64EncodedString()
-    //    //    }
-    //
-    
+    func calulateHmac(txn_type: String, amount: Int, store_id: String, order_id: String) -> String {
+        let zeroPadded = String(format: "%012d", amount)
+        let hmacData = txn_type
+        + zeroPadded
+        + store_id
+        + order_id
+        
+        return hmacData
+    }
 }
